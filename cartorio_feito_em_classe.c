cb390d6f447dc8cc253a7e5f9698b4d3d9f70dd4
Claudio Ferreira <56494623+Claduio0072004@users.noreply.github.com>
2024-01-25 08:57:10 -0300

    Modificações de Acessibilidade.
    Foi incluso uma serie de modificações pequenas como ""menus de retorno"", confirmação de escolha e uma opção de Alterar Dados já Cadastrados (está opção altera os dados do usuário já registrado no sistema).

--- cartorio_feito_em_classe.c
+++ cartorio_feito_em_classe.c
@@ -6,7 +6,10 @@
 int cadastro()
 {
 	
-	setlocale(LC_ALL, "Portuguese");
+	int opcao=0;
+	
+	system("cls"); // limpando a tela
+	
 	
 	char arquivo[40]; // definindo as strings
 	char cpf[40];
@@ -16,59 +19,162 @@ int cadastro()
 	
 	printf("\n\n\tInsira os dados que deseja Cadastrar.\n\n\n");
 	printf("\n\n   Digite o CPF para Cadastro:");
-	scanf("%s",cpf); // "%s" responsavel por salvar os valores dentro das strings
+	scanf("%s", &cpf); // "%s" responsavel por salvar os valores dentro das strings
+	
+	strcpy(arquivo, cpf); 
+	
+	FILE *file; //Não sei se foi a melhor forma mas foi a forma que encontrei de fazer a verificação:
+	file = fopen(arquivo,"r"); //Primeiro pedi para o computador pesquisar se havia algum arquivo com este CPF
+	
+		if(file == NULL) //Usei o IF...ELSE para deixar uma verificação se já estiver um usuário cadastrado
+		{
+		fclose(file); // Caso não enconte um usuário cadastrado o arquivo é fechado e depois reaberto para salvar.
+		
+		file = fopen(arquivo,"w"); // abrindo um novo arquivo e com o "w" escrevendo as informações dentro dele
+		fprintf(file,"\n CPF: ");
+		fprintf(file,cpf); //salva o arquivo
+		fclose(file); // fecha o arquivo
+	
+		file = fopen(arquivo,"a");
+		fprintf(file,".\n NOME: ");
+		fclose(file);
+	
+		printf("\n\n   Digite o Nome para ser Cadastrado:");
+		scanf("%s", &nome);
+	
+		file = fopen(arquivo,"a");
+		fprintf(file,nome);
+		fclose(file);
+	
+		file = fopen(arquivo,"a");
+		fprintf(file,".\n SOBRENOME: ");
+		fclose(file);
+	
+		printf("\n\n   Digite o Sobrenome a ser Cadastrado:");
+		scanf("%s", &sobrenome);
+	
+		file = fopen(arquivo,"a");
+		fprintf(file,sobrenome);
+		fclose(file);
+	
+		file = fopen(arquivo,"a");
+		fprintf(file,".\n CARGO: ");
+		fclose(file);
+	
+		printf("\n\n   Digite o Cargo que deseja Cadastrar:");
+		scanf("%s", &cargo);
+	
+		file = fopen(arquivo,"a");
+		fprintf(file,cargo);
+		fclose(file);
+		
+		file = fopen(arquivo,"a");
+		fprintf(file, ".\n\n");
+		fclose(file);
+		
+		system("cls");	//Aqui eu fiz uma limpa na tela para não haver uma quantidade muito grande de informações para o aviso que irá aparacer
+		
+		cadastro2(); // levando para o menu de retorno
+		
+		} //Aqui está o fim da verificação e claro caso não haja um usuário registrado o registro ocorre normalmente
+		else //Agora caso haja um usuário registrado A mesagem seguir irá aparecer
+		
+		system("cls");
+		
+		printf("\n\n\n\t      Usuário Já Cadastrado!\n\n\n");
+		printf("\n\n   Estamos te Encaminhando de Volta para o Menu.\n\n\n"); //Esta mensagem de retorno aparece em ambos os casos quando chega no fim da operação por isso achei bom limpar a tela acima
+		system("pause"); 
+		main();
+}
+
+int alterar() //achei uma boa ideia adicinar algo para alterar os dados ja cadastrados para que se o usuário tenha errado no registro ele não precise deletar e registrar novamente ou se o sobrenome da pssoa mudou também fique mais facil a alteração
+{
+	
+	system("cls");
+	
+	char arquivo[40];
+	char cpf[40];
+	char nome[40];
+	char sobrenome[40];
+	char cargo[40];
+	
+	
+	printf("\n\n\tInsira os dados para Alterar o Cadastro.\n\n\n");
+	printf("\n\n   Digite o CPF:  ");
+	scanf("%s", &cpf);
 	
 	strcpy(arquivo, cpf); //responsavel por copiar o valor das strings
 	
-	FILE *file; //criando arquivo no banco de dados
-	file = fopen(arquivo,"w");
-	fprintf(file,cpf); //salva o arquivo
-	fclose(file); // fecha o arquivo
+	FILE *file; 
+	file = fopen(arquivo,"r");
 	
-	file = fopen(arquivo,"a");
-	fprintf(file,"|");
-	fclose(file);
+		if (file)
+		{
+		fclose(file);
+		
+		file = fopen(arquivo,"w");
+		fprintf(file,"\n CPF: ");
+		fprintf(file, cpf); //salva o arquivo
+		fclose(file); // fecha o arquivo
 	
-	printf("\n\n   Digite o Nome para ser Cadastrado:");
-	scanf("%s",nome);
+		file = fopen(arquivo,"a");
+		fprintf(file,".\n NOME: ");
+		fclose(file);
 	
-	file = fopen(arquivo,"a");
-	fprintf(file,nome);
-	fclose(file);
+		printf("\n\n   Digite o Nome para ser Alterado:");
+		scanf("%s", &nome);
 	
-	file = fopen(arquivo,"a");
-	fprintf(file,"_");
-	fclose(file);
+		file = fopen(arquivo,"a");
+		fprintf(file,nome);
+		fclose(file);
 	
-	printf("\n\n   Digite o Sobrenome a ser Cadastrado:");
-	scanf("%s",sobrenome);
+		file = fopen(arquivo,"a");
+		fprintf(file,".\n SOBRENOME: ");
+		fclose(file);
 	
-	file = fopen(arquivo,"a");
-	fprintf(file,sobrenome);
-	fclose(file);
+		printf("\n\n   Digite o Sobrenome a ser Alterado:");
+		scanf("%s", &sobrenome);
 	
-	file = fopen(arquivo,"a");
-	fprintf(file,"|");
-	fclose(file);
+		file = fopen(arquivo,"a");
+		fprintf(file, sobrenome);
+		fclose(file);
 	
-	printf("\n\n   Digite o Cargo que deseja Cadastrar:");
-	scanf("%s",cargo);
+		file = fopen(arquivo,"a");
+		fprintf(file,".\n CARGO: ");
+		fclose(file);
 	
-	file = fopen(arquivo,"a");
-	fprintf(file,cargo);
-	fclose(file);
+		printf("\n\n   Digite o Cargo que deseja Alterado:");
+		scanf("%s", &cargo);
 	
-	system("pause");
+		file = fopen(arquivo,"a");
+		fprintf(file, cargo);
+		fclose(file);
+		
+		file = fopen(arquivo,"a");
+		fprintf(file, ".\n\n");
+		fclose(file);
+		
+		printf("\n\n");
+		system("pause");
+		
+		alterar2(); // levando para o menu de retorno
+		}
+		
+		if (file == NULL)
+		{
+			printf("\n\n\n\tO Usuário não está registrado .\n\n\n");
+			printf("\n\n  Estamos te Encaminhando de Volta para o Menu.\n\n\n");
+			system("pause");
+		}
 }
 
 int consulta()
 {
-	
-	setlocale(LC_ALL, "Portuguese");
+
+	system("cls");
 	
 	char cpf[40];
-	char conteudo[200];
-	
+	char conteudo[200];	
 	
 	printf("\n\n\tInsira o CPF do Usuário que deseja Consultar.\n\n\n");
 	printf("  CPF:");
@@ -79,41 +185,182 @@ int consulta()
 	
 	if(file == NULL)
 	{
-		printf("\n\n  Não foi encontrado o arquivo desejado\n\n");
+		fclose(file);
+		printf("\n\n  Não foi encontrado o arquivo desejado.\n\n\n");
+		system("pause");
+		main();
 	}
+	
+	system("cls");
+	
+	printf("\n\n\tEssas são as Informações do Usuário.\n");
 	
 	while (fgets(conteudo, 200, file) != NULL)
 	{
-		printf("\n\n\tEssas são as Informações do Usuário.");
-		printf("\n\n  CPF|Nome_Sobrenome|Cargo:  ");
-		printf("%s", conteudo);
-		printf("\n\n");
+		printf("%s",conteudo);
 	}
 	
+	fclose(file);
 	system("pause");
+	consulta2(); // levando para o menu de retorno
+	
 	
 }
 
 int deletar()
-{
+{	
+	int opcao=1;
+
 	char cpf[40];
+	
+	system("cls");
 	
 	printf("\n\n\tInsira o CPF do Usuário para Deletalo.\n\n\n");
 	printf("\n\n  CPF:  ");
-	scanf("%s",cpf);
-	
-	remove(cpf);
+	scanf("%s", &cpf);
+			
+	system("cls");
 	
 	FILE *file;
 	file = fopen(cpf,"r");
+	fclose(file);
 	
 	if(file == NULL)
 	{
-		printf("\n\n\tUsuário Não Encontrado!\n\n\n");
+		printf("\n\n\n\tUsuário Não Encontrado no Sistema.\n\n\n");
 		printf("\n\n   Estamos te Encaminhando de Volta para o Menu.\n\n\n");
 		system("pause");
+		main();
 	}
+	else
+
 	
+	printf("\n\n\tTem certeza que quer Deletar o Usuário?\n");
+	printf("\n\n\n  Digite 1 para deletalo ou qualquer outro caractere para voltar ao menu.");
+	printf("\n\n  Digite: ");
+	
+	scanf("%d", &opcao);
+	
+	switch (opcao)
+	{
+		case 1:
+		remove(cpf);
+		deletar2(); // levando para o menu de retorno
+		break;
+	
+		default:
+		main();
+		break;
+	}
+	 	
+}
+
+//Aqui em abaixo tem os ""MENUS"" de retorno para as opções que foram realizadas com sucesso fiz desta forma pois achei que não pesaria tanto no código 
+// Todos os ""MENUS"" de retorno sengue a mesma logica simples mas que aparentemente bem pratica
+
+int cadastro2()
+{     int opcao=0;
+     
+     system("cls");
+     
+     setlocale(LC_ALL, "Portuguese");
+	 
+	 	printf("\n\n\n\tUsuário Cadastrado com Sucesso.\n\n\n");
+		printf("\n\n   Deseja Cadastrar um novo Usuário?\n\n\n");
+		printf("\n\n Digite 1 para Cadastrar, ou outro caractere para Voltar ao Menu:");
+	
+		scanf("%d", &opcao);
+		
+		switch (opcao)
+	{
+		case 1:
+		cadastro();
+		break;
+	
+		default:
+		main();
+		break;
+	
+	}
+}
+
+int alterar2()
+{
+	     int opcao=0;
+     
+     system("cls");
+     
+     setlocale(LC_ALL, "Portuguese");
+	 
+	 	printf("\n\n\n\tAlteração de Cadastro Concluida!!\n\n\n");
+		printf("\n\n   Deseja Alterar o Cadastro de outro Usuário?\n\n\n");
+		printf("\n\n Digite 1 para Alterar, ou outro caractere para Voltar ao Menu:");
+	
+		scanf("%d", &opcao);
+		
+		switch (opcao)
+	{
+		case 1:
+		alterar();
+		break;
+	
+		default:
+		main();
+		break;
+	
+	}
+}
+
+int consulta2()
+{
+	int opcao=0;
+     
+    system("cls");
+    
+    setlocale(LC_ALL, "Portuguese");
+	 
+	printf("\n\n\n\tDeseja Consultar outro Usuário?\n\n\n");
+	printf("\n\n Digite 1 para Consultar, ou outro caractere para Voltar ao Menu:");
+	
+	scanf("%d", &opcao);
+		
+		switch (opcao)
+	{
+		case 1:
+		consulta();
+		break;
+	
+		default:
+		main();
+		break;
+	
+	}
+}
+
+int deletar2()
+{
+     int opcao=0;
+     
+     system("cls");
+     
+     setlocale(LC_ALL, "Portuguese");
+	 
+	 	printf("\n\n\n\tUsuário Deletado do Sistema.\n\n\n");
+		printf("\n\n   Deseja Deletar outro Usuário?\n\n\n");
+		printf("\n\n Digite 1 para Continuar ou outro caractere para Voltar ao Menu:");
+	
+		scanf("%d", &opcao);
+		
+		switch (opcao)
+	{
+		case 1:
+		deletar();
+		break;
+	
+		default:
+		main();
+		break;
+	}
 }
 
 int main()
@@ -130,10 +377,11 @@ int main()
         
 	    printf("\n\t### Cartório da EBAC ###\n\n"); //O começo do menu
 	    printf("   Escolha a Opção Desejada no Menu:\n\n");
-	    printf("\t1 - Registro de Usuários\n");
-	    printf("\t2 - Consultar Usuário\n");
-	    printf("\t3 - Deletar Usuário\n");
-	    printf("\t4 - Sair do Sistema\n\n\n");
+	    printf("\t1 - Registro de Usuários.\n");
+	    printf("\t2 - Alterar dados do Usuário.\n");
+	    printf("\t3 - Consultar Usuário.\n");
+	    printf("\t4 - Deletar Usuário.\n");
+	    printf("\t5 - Sair do Sistema.\n\n\n");
 	    printf("  Opção:"); //Final do menu
 	    
 	    scanf("%d", &opcao); //Armazenando Informações
@@ -145,22 +393,26 @@ int main()
 	        case 1:
 	        cadastro();
 	        break;
-	       
-		    case 2:
+	        
+	        case 2:
+	        alterar();
+		    break;
+	    				    // switch de seleção da operação desejada
+		    case 3:
 	        consulta();
 		    break;
-		                      // switch de seleção da operação desejada
-		    case 3:
+		
+		    case 4:
 		    deletar();
 		    break;
 		    
-		    case 4:
+		    case 5:
 		    printf("\n\n\n\n\tObrigado por Usar o Sistema de Cartório EBAC!!\n\n\n");
 		    return 0;
 		    break;
 		    
 		    default:
-		    printf("\n\n\tOpção invalida tente novamente!\n\n\n");
+		    printf("\n\n\n\n\tOpção invalida tente novamente!\n\n\n");
 	    	system("pause");
 			break;	 	
     	} //Aparentemente o fim das opções =)
